f1d472b74bae120a89a9bd07f9a1bcb1d5a01a5e
arthurgaier <90358599+arthurgaier@users.noreply.github.com>
2022-09-22 17:42:24 -0300

    projeto

--- projetoEstudo/adivinhacao.py
+++ projetoEstudo/adivinhacao.py
@@ -3,24 +3,26 @@ print("Bem vindo ao jogo da Adivinhação!")
 print("*********************************")
 
 numero_secreto = 50
+tentativas = 3
+rodada = 1
 
-userguess_str = input("Digite o seu número: ")
-userguess = int(userguess_str)
+while (rodada <= 3):
+    print("Tentativa", rodada, "de", tentativas)
+    userguess_str = input("Digite o seu número: ")
+    userguess = int(userguess_str)
 
-print("Voce digitou", userguess_str)
+    print("Voce digitou", userguess_str)
 
-acertou = userguess == numero_secreto
-maior   = userguess > numero_secreto
-menor   = userguess < numero_secreto
+    acertou = userguess == numero_secreto
+    maior   = userguess > numero_secreto
+    menor   = userguess < numero_secreto
 
-if (acertou):
-    print("Voce acertou!")
-else:
-    if(maior):
-        print("Voce errou! O seu chute foi maior que o numero secreto.")
-    elif(menor):
-        print("Voce errou! o seu chute foi menor que o numero secreto.")
+    if (acertou):
+        print("Voce acertou!")
+    else:
+        if(maior):
+            print("Voce errou! O seu chute foi maior que o numero secreto.")
+        elif(menor):
+            print("Voce errou! o seu chute foi menor que o numero secreto.")
 
-print("*********************************")
-print("Fim do jogo!")
-print("*********************************")
+    rodada = rodada + 1
