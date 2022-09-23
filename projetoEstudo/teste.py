9c21549d66a5d7126de67f6954b87845347516bd
arthurgaier <90358599+arthurgaier@users.noreply.github.com>
2022-09-23 01:14:30 -0300

    Update teste.py

--- projetoEstudo/teste.py
+++ projetoEstudo/teste.py
@@ -1,2 +1,16 @@
-for numero in range(1, 30 + 1):
-    print(numero)
+print("Bem vindo a Calculadora de Notas")
+
+
+media_str = input("Primeira nota:")
+media = int(media_str)
+media2_str = input("Segunda nota:")
+media2 = int(media2_str)
+media3_str = input("Terceira nota:")
+media3 = int(media3_str)
+media4_str = input("Quarta nota:")
+media4 = int(media4_str)
+
+mediafinal = media + media2 + media3 + media4
+mediafinal2 = mediafinal / 4
+
+print("Sua média final é: {}". format(mediafinal2))
